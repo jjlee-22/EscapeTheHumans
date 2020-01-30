b838cf369c6ff7c42a8aa87259e7c6b06d60655a
Eric DAlessandro <edalessandro22@jcu.edu>
2020-01-30 13:13:50 -0500

    Basic Main menu UI and script

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    private string Url = "https://adaa.org/understanding-anxiety/social-anxiety-disorder";
+
+    // Loads the desired game scene.
+    public void PlayGame()
+    {
+        // This will load the next scene in the build order. This can be changed to load a specific scene.
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    // Loads the scene containing instructions on how to play the game.
+    public void HowToPlayGame()
+    {
+        // This loads a scene called "HowToPlay" which contains instructions.
+        //TODO: Correct this name
+        SceneManager.LoadScene("HowToPlay");
+    }
+
+    // Loads the ADAA Page on Social Anxiety.
+    public void AnxietyInfo()
+    {
+        Application.OpenURL(Url);
+    }
+
+    // Rage Quit
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
