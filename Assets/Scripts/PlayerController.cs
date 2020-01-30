72b5488e6f5a07b5bd7524f49b68818b889fa93b
Eric DAlessandro <edalessandro22@jcu.edu>
2020-01-30 10:23:19 -0500

    Added scripts for camera movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float speed;
+}
+
+private Void Start()
+{
+    rb = GetComponent<Rigidbody>();
+}
+
+private void FixedUpdate()
+{
+    // Gets input for movement
+    float moveHorizonal = Input.GetAxis("Horizontal");
+    float moveVertical = Input.GetAxis("Vertical");
+
+    Vector3 movement = new Vector3(moveHorizonal, 0.0f, moveVertical);
+
+    // Applies movement to the rigidfbody
+    rb.AddForce(movement*speed);
+}
