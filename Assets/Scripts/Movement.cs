8ed22e9731a036330589eb990f1de6a519c268c7
jonlee96 <jjlee015@gmail.com>
2020-02-15 17:01:58 -0500

    Test git code refactoring

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -14,12 +14,12 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-	Vector3 movement= new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0.0f);
+	    Vector3 movement= new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0.0f);
 
-	animator.SetFloat("Horizontal", movement.x);
-	animator.SetFloat("Vertical", movement.y);
-	animator.SetFloat("Magnitude", movement.magnitude);
+	    animator.SetFloat("Horizontal", movement.x);
+	    animator.SetFloat("Vertical", movement.y);
+	    animator.SetFloat("Magnitude", movement.magnitude);
 
-	transform.position= transform.position+movement* Time.deltaTime*10;
+	    transform.position= transform.position+movement* Time.deltaTime*10;
     }
 }
