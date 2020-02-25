a81eea76e43c2416ddac4a6ce711e6e3d1a91ab0
AshG21jcu <agianguzzo21@jcu.edu>
2020-02-25 10:46:20 -0500

    AI animation started
    All animations not appearing when they should but they are made

--- Assets/Scripts/Patrol.cs
+++ Assets/Scripts/Patrol.cs
@@ -10,17 +10,24 @@ public class Patrol : MonoBehaviour
 
     public Transform[] movespots;
     private int randomspot;
+
+    public Animator animator;
     // Start is called before the first frame update
     void Start()
     {
         randomspot = Random.Range(0, movespots.Length);
         waitTime = startwaittime;
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.position = Vector2.MoveTowards(transform.position, movespots[randomspot].position, speed * Time.deltaTime);
+        Vector3 movement = new Vector3(transform.position.x, transform.position.y, 0.0f);
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Magnitude", movement.magnitude);
 
         if (Vector2.Distance(transform.position, movespots[randomspot].position) < 0.2f)
         {
